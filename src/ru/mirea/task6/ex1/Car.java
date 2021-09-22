f509603a1c7441ce3805b681cbc45237057d08e2
CTAC96A1 <stascepliaev@gmail.com>
2021-09-22 23:02:01 +0300

    .

---
+++ src/ru/mirea/task6/ex1/Car.java
@@ -0,0 +1,9 @@
+package ru.mirea.task6.ex1;
+
+public class Car implements Nameable
+{
+    public String getName()
+    {
+        return "Car";
+    }
+}
---
+++ src/ru/mirea/task6/ex1/NameableTester.java
@@ -0,0 +1,11 @@
+package ru.mirea.task6.ex1;
+
+public class NameableTester
+{
+    public static void main(String[] args) {
+        Car car = new Car();
+        Earth earth = new Earth();
+        System.out.println(car.getName());
+        System.out.println(earth.getName());
+    }
+}
