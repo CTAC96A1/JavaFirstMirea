27979ea1e7817dfd68666c182d38a9200928934f
CTAC96A1 <stascepliaev@gmail.com>
2021-10-07 00:40:43 +0300

    .

---
+++ src/ru/mirea/task8/Ex1/Circle.java
@@ -0,0 +1,13 @@
+package ru.mirea.task8.Ex1;
+
+public class Circle extends Shape{
+    private double radius = 1.0f;
+
+    public Circle(){}
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+}
---
+++ src/ru/mirea/task8/Ex1/Square.java
@@ -0,0 +1,13 @@
+package ru.mirea.task8.Ex1;
+
+public class Square extends Shape{
+    private int width = 100;
+
+    public Square(){}
+
+    public Square(int width)
+    {
+        this.width = width;
+    }
+
+}
