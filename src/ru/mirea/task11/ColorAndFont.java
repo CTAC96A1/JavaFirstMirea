40f44e431be73da8606f7ee6d2e21a113a18cd03
CTAC96A1 <stascepliaev@gmail.com>
2021-11-04 15:45:33 +0300

    .

--- src/ru/mirea/task11/ColorAndFont.java
+++ src/ru/mirea/task11/ColorAndFont.java
@@ -4,69 +4,64 @@ package ru.mirea.task11;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.util.Random;
 
 public class ColorAndFont extends JFrame {
-    final Random random = new Random();
 
     JTextArea area = new JTextArea("Hello World");
     Font TNR = new Font("Times new roman", Font.BOLD, 15);
     Font SS = new Font("Sans Serif", Font.BOLD, 15);
     Font CN = new Font("Courier New", Font.BOLD, 15);
-    JRadioButton r1 = new JRadioButton("Times new roman");
-    JRadioButton r2 = new JRadioButton("Sans Serif");
-    JRadioButton r3 = new JRadioButton("Courier New");
-    JRadioButton r4 = new JRadioButton("Blue color");
-    JRadioButton r5 = new JRadioButton("Red color");
-    JRadioButton r6 = new JRadioButton("Green color");
+    JRadioButton Style1 = new JRadioButton("Times new roman");
+    JRadioButton Style2 = new JRadioButton("Sans Serif");
+    JRadioButton Style3 = new JRadioButton("Courier New");
+    JRadioButton Color1 = new JRadioButton("Blue color");
+    JRadioButton Color2 = new JRadioButton("Red color");
+    JRadioButton Color3 = new JRadioButton("Green color");
 
     ColorAndFont() {
-        super("Choose options");
-
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
-
         panel.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30));
 
         panel.add(area);
-        panel.add(r1);
-        panel.add(r2);
-        panel.add(r3);
-        panel.add(r4);
-        panel.add(r5);
-        panel.add(r6);
-        frame.setSize(1000, 400);
+        panel.add(Style1);
+        panel.add(Style2);
+        panel.add(Style3);
+        panel.add(Color1);
+        panel.add(Color2);
+        panel.add(Color3);
+        frame.setSize(800, 100);
         frame.setLocationRelativeTo(null);
         frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
-        r1.addActionListener(new ActionListener() {
+        Style1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 area.setFont(TNR);
             }
         });
-        r2.addActionListener(new ActionListener() {
+        Style2.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                         area.setFont(SS);
             }
             });
-        r3.addActionListener(new ActionListener() {
+        Style3.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                         area.setFont(CN);
                     }
     });
-        r4.addActionListener(new ActionListener() {
+        Color1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 area.setForeground(Color.blue);
             }
         });
-        r5.addActionListener(new ActionListener() {
+        Color2.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 area.setForeground(Color.red);
             }
         });
-        r6.addActionListener(new ActionListener() {
+        Color3.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 area.setForeground(Color.green);
             }
